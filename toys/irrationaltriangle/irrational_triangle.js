29a75b4ae8c9c9df1ad3c08702e3c63c41deba68
Darius Bacon <darius@wry.me>
2013-02-25 13:29:13 -0500

    code cleanup

--- toys/irrationaltriangle/irrational_triangle.js
+++ toys/irrationaltriangle/irrational_triangle.js
@@ -1,29 +1,37 @@
 function irrationalTriangle(ctx, width, height) {
+    ctx.globalCompositeOperation = 'lighter';
+    ctx.fillStyle = 'rgba(0,0,255,0.1)';
+
     var size = Math.min(width, height);
     var sqrt3 = Math.sqrt(3);
     var h = (1 - sqrt3/2) / 2;
-    var colors = ['rgba(0,128,192,0.25)', 'rgba(0,128,192,0.5)', 'rgba(0,120,180,0.9)', 'rgba(0,80,120,0.99)',];
+    construct(1, [{x: .5, y: h+sqrt3/2},
+                  {x: 0,  y: h},
+                  {x: 1,  y: h}]);
 
-    construct([{x: .5, y: h+sqrt3/2},
-               {x: 0,  y: h},
-               {x: 1,  y: h}],
-              0);
-
-    function construct(triangle, color) {
+    function construct(count, triangle) {
         var crossing = [];
         for (var i = 0; i < 3; ++i) {
             var u = add(triangle[i], scale(1/sqrt3, sub(triangle[(i+1)%3], triangle[i])));
             var v = add(triangle[i], scale(1/sqrt3, sub(triangle[(i+2)%3], triangle[i])));
-            drawTriangle([triangle[i], u, v], colors[color % colors.length]);
+            for (var j = 0; j < count; ++j)
+                drawTriangle([triangle[i], u, v]);
             crossing.push([u, v]);
         }
-        if (0.32 < norm(triangle[0], triangle[1])) {
+        if (count < 16) {
             var central = [];
-            for (var j = 0; j < 3; ++j) {
-                central.push(intersect(crossing[j], crossing[(j+1)%3]));
-            }
-            construct(central, color+1);
+            for (var k = 0; k < 3; ++k)
+                central.push(intersect(crossing[k], crossing[(k+1)%3]));
+            construct(count*2, central);
         }
+    }
+
+    function drawTriangle(triangle) {
+        ctx.beginPath();
+        ctx.moveTo(size * triangle[0].x, size * triangle[0].y);
+        ctx.lineTo(size * triangle[1].x, size * triangle[1].y);
+        ctx.lineTo(size * triangle[2].x, size * triangle[2].y);
+        ctx.fill();
     }
 
     // Return the point where the lines through points1 and points2 coincide.
@@ -47,18 +55,4 @@ function irrationalTriangle(ctx, width, height) {
     function sub(u, v)      { return add(u, scale(-1, v)); }
     function scale(c, v)    { return {x: c*v.x, y: c*v.y}; }
     function add(u, v)      { return {x: u.x+v.x, y: u.y+v.y}; }
-
-    function drawTriangle(triangle, color) {
-//        ctx.strokeStyle = color;
-        ctx.globalCompositeOperation = 'lighter';
-        ctx.fillStyle = color;
-        ctx.beginPath();
-        ctx.moveTo(size * triangle[0].x, size * triangle[0].y);
-        ctx.lineTo(size * triangle[1].x, size * triangle[1].y);
-        ctx.lineTo(size * triangle[2].x, size * triangle[2].y);
-        ctx.fill();
-
-//        ctx.closePath();
-//        ctx.stroke();
-    }
 }
