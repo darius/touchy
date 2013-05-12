9c9ac71a3b2e21aac8dfba93dc338d55e95661f8
Darius Bacon <darius@wry.me>
2013-05-11 21:42:53 -0700

    translate from right-handed world coords; define vars for sail's attitude

--- t/gravity/gravity.js
+++ t/gravity/gravity.js
@@ -25,6 +25,9 @@ function step() {
 var xscale = 4; // -xscale..xscale is visible, in world coords
 var yscale = 4;
 
+var tx = 3/5;   // (tx,ty) is a unit vector, the sail's attitude
+var ty = 4/5;
+
 function plotMe() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, width, height);
@@ -33,8 +36,12 @@ function plotMe() {
     ctx.arc(width/2, height/2, 8, tau, false);
     ctx.fill();
     var cx = width/2 * (1 + x/xscale);  // canvas coords
-    var cy = height/2 * (1 + y/yscale);
-    drawLine({x: cx-10, y: cy-5}, {x: cx+10, y:cy+5}, 'white');
+    var cy = height/2 * (1 - y/yscale);
+    var atx = 10 * tx;
+    var aty = -10 * ty;
+    drawLine({x: cx-atx, y: cy-aty},
+             {x: cx+atx, y: cy+aty},
+             'white');
 }
 
 function drawLine(start, end, color) {
