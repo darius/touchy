f929100d0b08f5e99e2b4b181f1c4479517cfdbf
Darius Bacon <darius@wry.me>
2015-12-25 13:05:45 -0500

    add permalinks

--- tmp/flake.js
+++ tmp/flake.js
@@ -1,5 +1,7 @@
+'use strict';
+
 var ctx; // for debug
-function interact(canvas, report) {
+function interact(canvas, paths) {
     var width = 0+canvas.width;
     var height = 0+canvas.height;
     var ox = width/2;
@@ -10,7 +12,6 @@ function interact(canvas, report) {
     ctx.lineCap = 'butt';
 
     var endpoints = [];
-    var paths = [];
 
     function show() {
         ctx.clearRect(0, 0, width, height);
@@ -45,6 +46,7 @@ function interact(canvas, report) {
               y: xy.y-oy};
         paths[paths.length-1].push(xy);
         show();
+        permalink.href = encodeState(document.URL, paths);
     }
 
     function onMousedown(xy) {
@@ -61,19 +63,72 @@ function interact(canvas, report) {
 
     function onMouseup() {
         delete endpoints[0];
+        console.log(paths.reduce((sum, a) => sum + a.length, 0));
     }
 
     canvas.addEventListener('mousedown', pointing.leftButtonOnly(pointing.mouseHandler(canvas, onMousedown)));
     canvas.addEventListener('mousemove', pointing.mouseHandler(canvas, onMousemove));
     canvas.addEventListener('mouseup',   pointing.mouseHandler(canvas, onMouseup));
 
-    report('Starting');
+    show();
 }
 
-var canvas = document.getElementById('canvas');
-var debug = document.getElementById('debug');
-function report(html) {
-    if (false) debug.innerHTML += '<br>' + html;
+function encodeState(url, paths) {
+    var qmark = url.indexOf('?');
+    var base = qmark < 0 ? url : url.substring(0, qmark);
+    return base + '?s=' + encodeURIComponent(encodeScene(paths));
 }
 
-interact(canvas, report);
+function decodeParams(url) {
+    var result = {};
+    var qmark = url.indexOf('?');
+    if (0 <= qmark) {
+        var pairs = url.substring(qmark+1, url.length).split('&');
+        for (var i = 0; i < pairs.length; ++i) {
+	    var kv = pairs[i].split('=', 2);
+	    result[decodeURIComponent(kv[0])] = decodeURIComponent(kv[1]);
+        }
+    }
+    return result;
+}
+
+function encodeScene(paths) {
+    return paths.map(path => deltas(path.map(xy => xy.x)) + '~' + deltas(path.map(xy => xy.y))).join('/');
+}
+
+function deltas(points) {
+    var prev = 0.0;
+    return points.map(p => {
+        const delta = '' + (p - prev);
+        prev = p;
+        return delta;
+    }).join('_');
+}
+
+function deltaDecode(string) {
+    var acc = 0.0;
+    return string.split('_').map(d => acc += parseFloat(d));
+}
+
+function decodeScene(scene) {
+    try {
+        return scene.split('/').map(s => makeXYs(s.split('~').map(deltaDecode)));
+    } catch (e) {
+        // Because my decoding code is sloppy about syntax errors, we fall back to this:
+        console.log('Error decoding', e);
+        return [];
+    }
+}
+
+function makeXYs(coordArrays) {
+    var result = [];
+    var length = coordArrays[0].length;
+    for (var i = 0; i < length; ++i) {
+        result.push({x: coordArrays[0][i], y: coordArrays[1][i]});
+    }
+    return result;
+}
+
+var params = decodeParams(document.URL);
+if (params.s) interact(canvas, decodeScene(params.s))
+else          interact(canvas, []);
